80c7b11ea2d7f6bf401f4e1b87c2bdf69f4bcfb5
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-04-17 23:12:52 +0700

    arrow functions

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ let age = 29;
 const hasHobbies = true;
 
 age = 30;
-function summarizaUser(userName, userAge, userHasHobby){
+
+const summarizaUser = (userName, userAge, userHasHobby) => {
     return `Name is ${userName}, age is ${userAge}, and the user has hobbies: ${userHasHobby}`;
 }
 
+// Add
+const add = (a, b) => a + b;
+// Add one
+const addOne = a => a + 1;
+// Add random
+const addRandom = () => 1 + 2;
+
+console.log(add(1,2));
+console.log(addOne(2));
+console.log(addRandom());
 console.log(summarizaUser(name, age, hasHobbies));
