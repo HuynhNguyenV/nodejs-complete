5a746b91011524b460e4df7155471618dc28c4ad
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-04-17 22:58:23 +0700

    let and const

---
+++ app.js
@@ -0,0 +1,12 @@
+const name = 'Max';
+let age = 29;
+const hasHobbies = true;
+
+age = 30;
+function summarizaUser(userName, userAge, userHasHobby){
+    return `Name is ${userName}, age is ${userAge}, and the user has hobbies: ${userHasHobby}`;
+}
+
+console.log(summarizaUser(name, age, hasHobbies));
+
+
